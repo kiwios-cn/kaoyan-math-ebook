899c3a2cee1bb5b3163c5ec8216775d1e5ad1e80
kiwios <kiwios.cn@gmail.com>
2026-08-05 15:04:24 +0800

    Bump cache for sequence page fix

--- ebook/sw.js
+++ ebook/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "20260805-image-problem-batch";
+const CACHE_VERSION = "20260805-sequence-page-fix";
 const SHELL_CACHE = `kaoyan-math-ebook-shell-${CACHE_VERSION}`;
 const PAGE_CACHE = `kaoyan-math-ebook-pages-${CACHE_VERSION}`;
 const ACTIVE_CACHES = new Set([SHELL_CACHE, PAGE_CACHE]);
@@ -6,9 +6,9 @@ const ACTIVE_CACHES = new Set([SHELL_CACHE, PAGE_CACHE]);
 const SHELL_ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=20260805-image-problem-batch",
-  "./data.js?v=20260805-image-problem-batch",
-  "./app.js?v=20260805-image-problem-batch",
+  "./style.css?v=20260805-sequence-page-fix",
+  "./data.js?v=20260805-sequence-page-fix",
+  "./app.js?v=20260805-sequence-page-fix",
 ];
 
 self.addEventListener("install", (event) => {
